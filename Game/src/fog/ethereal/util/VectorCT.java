d7ae2a1c5e141a4153aa078183395374dbec2801
EtherealFog <baldwinelias77@gmail.com>
2015-04-10 20:19:24 -0700

    changes to vector classes
    Signed-off-by: EtherealFog <baldwinelias77@gmail.com>

--- Game/src/fog/ethereal/util/VectorCT.java
+++ Game/src/fog/ethereal/util/VectorCT.java
@@ -1,7 +1,31 @@
 package fog.ethereal.util;
-
+/**
+ * Vector for cartesian coordinate system; 
+ * origin as 'tail' of vector with (x, y) coordinates of head.
+ */
 public class VectorCT {
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 	
+	public VectorCT(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public static VectorMD toMD(VectorCT ct) {
+		double m;
+		double d;
+		m = Math.sqrt(ct.getX() * ct.getX() + ct.getY() * ct.getY());
+		d = Math.toRadians(Math.atan2(ct.getX(), ct.getY()));
+		VectorMD md = new VectorMD(m, d);
+		return md;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
 }
--- Game/src/fog/ethereal/util/VectorMD.java
+++ Game/src/fog/ethereal/util/VectorMD.java
@@ -1,5 +1,19 @@
 package fog.ethereal.util;
 
 public class VectorMD {
-
+	/**
+	 * double representing magnitude
+	 */
+	double m;
+	/**
+	 * double representing standard rotation (starting on positive x-axis and going counter-clockwise)
+	 * in radians.
+	 */
+	double d;
+	
+	public VectorMD(double m, double d) {
+		this.m = m;
+		this.d = d;
+	}
+	
 }
