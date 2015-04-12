84446ded27649b450f46a920fb238f5e42665f39
EtherealFog <baldwinelias77@gmail.com>
2015-04-12 16:18:33 -0700

    Tentatively added Bike and Wheel classes (may be changed in the future).
    Signed-off-by: EtherealFog <baldwinelias77@gmail.com>

---
+++ Game/src/fog/ethereal/sprite/Wheel.java
@@ -0,0 +1,5 @@
+package fog.ethereal.sprite;
+
+public class Wheel {
+
+}
--- Game/src/fog/ethereal/world/Platform.java
+++ Game/src/fog/ethereal/world/Platform.java
@@ -1,7 +1,17 @@
 package fog.ethereal.world;
 
 import javafx.scene.shape.Line;
+import fog.ethereal.sprite.Wheel;
 
 public class Platform extends Line {
-
+	
+	public Platform() {
+		
+	}
+	
+	public boolean checkCollision(Wheel wheel) {
+		
+		
+		return false;
+	}
 }
